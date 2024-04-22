69cfff90683e7c0b24e9491dfe957b2afa2681ba
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-04-23 01:57:24 +0900

    Design #6 - configureUI 완료, 앱로고 이미지 크기 오류 수정

--- CinemaApp/Presentation/login/ViewController/LoginViewController.swift
+++ CinemaApp/Presentation/login/ViewController/LoginViewController.swift
@@ -40,17 +40,58 @@ class LoginViewController: UIViewController {
     
     
     // MARK: - UI Setting functions
+    func configureUI() {
+        setBackgroundUI()
+        setAppLogoUI()
+        setCornerRadius()
+        setFont()
+    }
+    
     func setupConstraints() {
         setTranslatesAutoresizingMaskIntoConstraintsFalse()
         setBackgroundConstraints()
         setLoginComponentsConstraints()
     }
    
-    func configureUI() {
-        setBackgroundUI()
-        setAppLogoUI()
+    
+    
+    // MARK: - configure UI
+    func setBackgroundUI() {
+        // 포스터 이미지
+        let image = UIImage(systemName: "viewfinder.circle.fill")
+        self.bgPosterImage.image = image
+        self.bgPosterImage.contentMode = .scaleAspectFill
+        
+        // 투명도 80% 검정으로 덮기
+        self.bgTransparentBlackImage.backgroundColor = UIColor.black
+        self.bgTransparentBlackImage.alpha = 0.8
     }
     
+    func setAppLogoUI() {
+        let image = UIImage(systemName: "apple.logo")
+        self.appLogoImage.tintColor = .white
+        self.appLogoImage.image = image
+        self.appLogoImage.contentMode = .scaleAspectFit
+    }
+    
+    func setCornerRadius() {
+        self.idBoxView.layer.cornerRadius = 30
+        self.pwBoxView.layer.cornerRadius = 30
+        self.loginButton.layer.cornerRadius = 30
+    }
+    
+    func setFont() {
+        [self.idLabel, 
+         self.pwLabel].forEach {
+            $0.font = UIFont.systemFont(ofSize: 18)
+        }
+        [self.searchIDButton.titleLabel,
+         self.registerButton.titleLabel].forEach {
+            $0?.font = UIFont.boldSystemFont(ofSize: 16)
+        }
+        self.autoLoginCheckLabel.font = UIFont.systemFont(ofSize: 16)
+        self.loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+    }
     
     
     // MARK: - Setup Constraints
@@ -90,18 +131,19 @@ class LoginViewController: UIViewController {
     
     func setLoginComponentsConstraints() {
         // 컴포넌트 엄마뷰: bottom, height, horizontalEdges
-        self.loginComponentsView.backgroundColor = .systemPink
+        //self.loginComponentsView.backgroundColor = .systemPink
         self.loginComponentsView.snp.makeConstraints {
             $0.bottom.equalToSuperview().inset(140) // bottom
             $0.height.equalTo(360) // height
             $0.horizontalEdges.equalToSuperview().inset(35)
         }
         
-        // 앱 로고: top, height, centerX
+        // 앱 로고: top, height, centerX, width
         self.appLogoImage.snp.makeConstraints {
             $0.top.equalTo(loginComponentsView.snp.top) // top
             $0.height.equalTo(40) // height
             $0.centerX.equalTo(loginComponentsView) // centerX
+            $0.width.equalTo(loginComponentsView) // width
         }
         
         // id Box: top, height, horizontalEdges
@@ -130,23 +172,26 @@ class LoginViewController: UIViewController {
             $0.centerY.equalTo(pwBoxView)
         }
         
-        // 자동로그인 뷰: top, height, trailing
+        // 자동로그인 뷰: top, height, trailing, width
+        self.autoLoginCheckView.backgroundColor = .brown
         self.autoLoginCheckView.snp.makeConstraints {
             $0.top.equalTo(pwBoxView.snp.bottom).offset(10)
             $0.height.equalTo(26)
             $0.trailing.equalTo(loginComponentsView.snp.trailing)
+            $0.width.equalTo(loginComponentsView)
         }
         
-        // 자동로그인 label: centerY, trailing
+        // 자동로그인 label: centerY, trailing, width
         self.autoLoginCheckLabel.snp.makeConstraints {
             $0.centerY.equalTo(autoLoginCheckView)
-            //$0.trailing.equalTo(autoLoginCheckView.snp.trailing)
+            $0.trailing.equalTo(autoLoginCheckView.snp.trailing)
+            $0.width.greaterThanOrEqualTo(0)
         }
         
         // 자동로그인 버튼 : horizontalEdges, trailing
         self.autoLoginCheckButton.snp.makeConstraints {
             $0.horizontalEdges.equalTo(autoLoginCheckView)
-            $0.trailing.equalTo(autoLoginCheckLabel.snp.leading).inset(3)
+            $0.trailing.equalTo(autoLoginCheckLabel.snp.leading).inset(2)
         }
         
         // 로그인 버튼: top, height, horizontalEdges
@@ -174,23 +219,7 @@ class LoginViewController: UIViewController {
     
     
     
-    // MARK: - configure UI
-    func setBackgroundUI() {
-        // 포스터 이미지
-        let image = UIImage(systemName: "photo.fill")
-        self.bgPosterImage.image = image
-        self.bgPosterImage.contentMode = .scaleAspectFill
-        
-        // 투명도 80% 검정
-        self.bgTransparentBlackImage.backgroundColor = UIColor.black
-        self.bgTransparentBlackImage.alpha = 0.8
-    }
-    
-    func setAppLogoUI() {
-        let image = UIImage(systemName: "apple.logo")
-        self.appLogoImage.image = image
-        self.appLogoImage.contentMode = .scaleAspectFit
-    }
+
     
     
 }
