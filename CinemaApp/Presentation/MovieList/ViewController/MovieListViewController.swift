23901c6f3d14a87e0db29c36d4a3233cd4717d3b
devpark435 <devpark435@gmail.com>
2024-04-22 20:32:29 +0900

    Design #7 - Update MovieList UI Design

--- CinemaApp/Presentation/MovieList/ViewController/MovieListViewController.swift
+++ CinemaApp/Presentation/MovieList/ViewController/MovieListViewController.swift
@@ -8,13 +8,77 @@
 import UIKit
 
 class MovieListViewController: UIViewController {
-
+    
+    @IBOutlet weak var categorySegementController: UISegmentedControl!
+    @IBOutlet weak var movieCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = UIColor(named: "backgroundColor")
+        setupCollectionView()
+        setupSegement()
     }
-
-
+    
+    func setupCollectionView() {
+        movieCollectionView.layer.cornerRadius = 24
+        movieCollectionView.layer.masksToBounds = true
+        movieCollectionView.delegate = self
+        movieCollectionView.dataSource = self
+        movieCollectionView.collectionViewLayout = UICollectionViewFlowLayout().then {
+            $0.scrollDirection = .horizontal
+            $0.minimumLineSpacing = 0
+            $0.minimumInteritemSpacing = 0
+        }
+    }
+    
+    func setupSegement(){
+        // 기존 세그먼트 제거
+        categorySegementController.removeAllSegments()
+        
+        // 새로운 세그먼트 추가 및 텍스트 설정
+        categorySegementController.insertSegment(withTitle: "Playing", at: 0, animated: false)
+        categorySegementController.insertSegment(withTitle: "Popular", at: 1, animated: false)
+        categorySegementController.insertSegment(withTitle: "TopRated", at: 2, animated: false)
+        categorySegementController.insertSegment(withTitle: "UpComing", at: 3, animated: false)
+        
+        // 세그먼트 텍스트 스타일 설정
+        let normalTextAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 15),
+            .foregroundColor: UIColor.white
+        ]
+        categorySegementController.setTitleTextAttributes(normalTextAttributes, for: .normal)
+        
+        let selectedTextAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.boldSystemFont(ofSize: 15),
+            .foregroundColor: UIColor.black
+        ]
+        categorySegementController.setTitleTextAttributes(selectedTextAttributes, for: .selected)
+        
+        // 첫 번째 세그먼트를 선택된 상태로 설정
+        categorySegementController.selectedSegmentIndex = 0
+    }
+    
 }
 
+extension MovieListViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCollectionViewCell", for: indexPath) as? MovieCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(image: UIImage(named: "placeholder"), title: "Title", description: "description")
+        return cell
+    }
+}
+
+extension MovieListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // 원하는 셀의 크기를 반환합니다.
+        return CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height)
+    }
+}
