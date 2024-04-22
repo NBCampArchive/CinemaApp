25f6156b881d3caa86b20ceaea6a022e769365b3
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-04-22 18:05:22 +0900

    Design #6 - 스토리보드 UI Components 계층 수정

---
+++ CinemaApp/Presentation/login/ViewController/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  CinemaApp
+//
+//  Created by 유림 on 4/22/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+}
