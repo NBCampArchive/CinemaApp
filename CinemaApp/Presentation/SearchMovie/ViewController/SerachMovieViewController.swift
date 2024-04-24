2a960c1c95188b2ade859e18d3ebb4e3b3b130f0
yeggrrr <yjkim981103@gmail.com>
2024-04-24 20:47:07 +0900

    searchBar IU 수정 및 noticeLabel  & 기능 추가

--- CinemaApp/Presentation/SearchMovie/ViewController/SerachMovieViewController.swift
+++ CinemaApp/Presentation/SearchMovie/ViewController/SerachMovieViewController.swift
@@ -11,7 +11,8 @@ import Kingfisher
 class SearchMovieViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var movieListCollectionView: UICollectionView!
-
+    @IBOutlet weak var noticeLabel: UILabel!
+    
     // TODO: Key를 발급받아 채워주세요.
     let authenticationKey = ""
     let urlString = "https://api.themoviedb.org/3/search/movie"
@@ -31,15 +32,51 @@ class SearchMovieViewController: UIViewController, UICollectionViewDelegate, UIC
         movieListCollectionView.dataSource = self
         movieListCollectionView.delegate = self
         view.backgroundColor = UIColor(named: "BackgroundColor")
-        getData(query: "12")
+        getData(query: "") { a in }
         setSearchBar()
+        noticeLabelUI()
+        
     }
     
+    func noticeLabelUI() {
+        noticeLabel.text = "검색어 없음"
+        noticeLabel.font = UIFont.systemFont(ofSize: 20)
+        noticeLabel.textColor = UIColor(named: "customPrimaryColor")
+        noticeLabel.isHidden = false
+        
+    }
+    
+    // TODO: x 버튼 컬러 잘보이도록 바꾸기 / 입력값 없을 때, 해당 입력값 없을 때 나오는 문구 만들기
     func setSearchBar() {
         searchBar.placeholder = "영화 제목을 검색하세요."
+        searchBar.setImage(UIImage(named: "icSearchNonW"), for: UISearchBar.Icon.search, state: .normal)
+        searchBar.setImage(UIImage(named: "icCancel"), for: .clear, state: .normal)
+        searchBar.barTintColor = UIColor(named: "customPrimaryColor")
+        // cancel button color 변경
+        let cancelButtonAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+         UIBarButtonItem.appearance().setTitleTextAttributes(cancelButtonAttributes , for: .normal)
+        
+        if let textfield = searchBar.value(forKey: "searchField") as? UITextField {
+            textfield.backgroundColor = UIColor(named: "customPrimaryColor")
+            textfield.textColor = UIColor(named: "LabelTextColor")
+            
+            textfield.attributedPlaceholder = NSAttributedString(string: textfield.placeholder ?? "", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+            
+            textfield.textColor = UIColor.white
+            //왼쪽 아이콘 이미지
+            if let leftView = textfield.leftView as? UIImageView {
+                leftView.image = leftView.image?.withRenderingMode(.alwaysTemplate)
+                leftView.tintColor = UIColor.white
+            }
+            //오른쪽 x버튼 이미지
+            if let rightView = textfield.rightView as? UIImageView {
+                rightView.image = rightView.image?.withRenderingMode(.alwaysTemplate)
+                rightView.tintColor = UIColor.white
+            }
+        }
     }
     
-    func getData(query: String) {
+    func getData(query: String, completion: @escaping([Results]) -> Void ) {
         guard let url = URL(string: urlString) else { return }
         // 파라미터 or 헤더를 추가 -> URLComponents -> URLRequest 안에 components?.url 넣기
         // 헤더 or 파라미터 필요 x -> URLRequest만 생성
@@ -65,6 +102,7 @@ class SearchMovieViewController: UIViewController, UICollectionViewDelegate, UIC
                 let decoder = JSONDecoder()
                 let result = try decoder.decode(Movies.self, from: data)
                 self.movieList = result.results
+                completion(result.results)
             } catch {
                 print("error: \(error)")
             }
@@ -122,12 +160,25 @@ class SearchMovieViewController: UIViewController, UICollectionViewDelegate, UIC
 extension SearchMovieViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         guard let searchText = searchBar.text else { return }
-        getData(query: searchText)
+        getData(query: searchText) { movieResults in
+            if self.movieList.count == 0 && searchText != "" {
+                DispatchQueue.main.async {
+                    self.noticeLabel.isHidden = false
+                    self.noticeLabel.text = "이런! 찾으시는 작품이 없습니다."
+                    self.noticeLabel.textColor = UIColor(named: "customPrimaryColor") }
+            } else if self.movieList.count != 0 {
+                DispatchQueue.main.async {
+                    self.noticeLabel.isHidden = true }
+            } else {
+                DispatchQueue.main.async {
+                    self.noticeLabel.isHidden = false
+                    self.noticeLabel.text = "검색어 없음"
+                    self.noticeLabel.textColor = UIColor(named: "customPrimaryColor") }
+            }
+        }
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         view.endEditing(true)
     }
 }
-
-
