d7993e1ca1c629024a88b2bc48af129254285441
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-04-25 18:08:42 +0900

    design #26 - StoryBoard에서 레이아웃 설정 완료

---
+++ CinemaApp/Presentation/MyPage/ViewController/MyPageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MyPageViewController.swift
+//  CinemaApp
+//
+//  Created by 유림 on 4/25/24.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+}
