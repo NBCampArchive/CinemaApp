eebe8e1ad9f7a5d939ef469f2f63163893fc2717
HyunRyeol Park <devpark435@gmail.com>
2024-04-26 22:36:28 +0900

    feat #40 - Reservation List TableView

--- CinemaApp/Presentation/MyPage/ViewController/ReservationListViewController.swift
+++ CinemaApp/Presentation/MyPage/ViewController/ReservationListViewController.swift
@@ -5,4 +5,55 @@
 //  Created by 박현렬 on 4/26/24.
 //
 
-import Foundation
+import UIKit
+
+class ReservationListViewController: UIViewController{
+    
+    @IBOutlet weak var reservationListTableView: UITableView!
+    
+    var reservationList: [Reservation] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        reservationListTableView.dataSource = self
+        reservationListTableView.delegate = self
+        
+        view.backgroundColor = UIColor(named: "BackgroundColor")
+        reservationListTableView.backgroundColor = UIColor(named: "BackgroundColor")
+        
+        navigationItem.title = "예매 내역"
+        
+        reservationList = loadReservations()
+    }
+    
+    func loadReservations() -> [Reservation] {
+        if let data = UserDefaults.standard.data(forKey: "reservations") {
+            let decoder = JSONDecoder()
+            if let reservations = try? decoder.decode([Reservation].self, from: data) {
+                return reservations
+            }
+        }
+        return []
+    }
+}
+
+extension ReservationListViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reservationList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ReservationListViewCell", for: indexPath) as? ReservationListViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.configure(reservation: reservationList[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+}
