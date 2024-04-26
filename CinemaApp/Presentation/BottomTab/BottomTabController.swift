1e2176dc0c89486b3f1ffea36e36448a61da4b1c
HyunRyeol Park <devpark435@gmail.com>
2024-04-26 23:21:20 +0900

    feat #40 - refactor rootView

--- CinemaApp/Presentation/BottomTab/BottomTabController.swift
+++ CinemaApp/Presentation/BottomTab/BottomTabController.swift
@@ -11,15 +11,22 @@ class BottomTabController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationController?.isNavigationBarHidden = true
         setupViewControllers()
+        UITabBar.appearance().tintColor = UIColor(named: "LabelTextColor")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true
     }
     
     private func setupViewControllers() {
         // 첫 번째 탭 - MovieListViewController
-        let movieListViewController = UIStoryboard(name: "MovieList", bundle: nil).instantiateViewController(withIdentifier: "MovieListViewController")
+        let movieListViewController = UIStoryboard(name: "MovieList", bundle: nil).instantiateViewController(withIdentifier: "MovieListViewController") as! MovieListViewController
         let movieListTabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
         movieListViewController.tabBarItem = movieListTabBarItem
+        
         
         // 두 번째 탭 - SearchViewController
         let searchViewController = UIStoryboard(name: "SearchMovie", bundle: nil).instantiateViewController(withIdentifier: "SerachMovieViewController")
@@ -32,12 +39,13 @@ class BottomTabController: UITabBarController {
         movieMapViewController.tabBarItem = movieMapTabBarItem
         
         // 네 번째 탭 - ProfileViewController
-        let profileViewController = UIStoryboard(name: "MyPage", bundle: nil).instantiateViewController(withIdentifier: "MyPageViewController")
+        let profileViewController = UIStoryboard(name: "MyPage", bundle: nil).instantiateViewController(withIdentifier: "MyPageViewController") as! MyPageViewController
         let profileTabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
         profileViewController.tabBarItem = profileTabBarItem
+        let navigationProfile = UINavigationController(rootViewController: profileViewController)
         
         // 뷰 컨트롤러 배열 생성
-        let viewControllers = [movieListViewController, searchViewController, movieMapViewController, profileViewController]
+        let viewControllers = [movieListViewController, searchViewController, movieMapViewController, navigationProfile]
         
         // 탭바 컨트롤러의 뷰 컨트롤러 설정
         self.setViewControllers(viewControllers, animated: false)
