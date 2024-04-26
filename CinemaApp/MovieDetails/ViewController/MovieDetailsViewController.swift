2a15d31674982a32b5c0339c573db51043a20def
taeng-sa <k01096752307@gmail.com>
2024-04-26 14:21:39 +0900

    상세페이지 데이터 연결시도 및 예약페이지 변경

--- CinemaApp/MovieDetails/ViewController/MovieDetailsViewController.swift
+++ CinemaApp/MovieDetails/ViewController/MovieDetailsViewController.swift
@@ -7,119 +7,95 @@
 
 import UIKit
 
-class MovieDetailsViewController: UIViewController {
+class MovieDetailsViewController: UIViewController, UIScrollViewDelegate {
     
-
+    
     
     @IBOutlet weak var detailimageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var releasedateLabel: UILabel!
     @IBOutlet weak var reservationButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     
-    private var randomNumber: Int32? {
-        (1...2147483647).randomElement()
-    }
-//    var scrollView: UIScrollView! {
-//            // 스크롤 뷰 생성
-//            let scrollView = UIScrollView(frame: CGRect(x: 0, y: 449.33, width: 393, height: 206))
-//            scrollView.backgroundColor = .lightGray
-//            return scrollView
-//    }
+    var movieDetail: MovieDetail?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "BackgroundColor")
         
-
-        if let movie_id = randomNumber,
-           let url = URL(string: "https://api.themoviedb.org/3/movie/\("movie_id")") {
-            //네트워크 요청
-            
-            let request = URLRequest(url: url)
-            
-            let task = URLSession.shared.dataTask(with: request) {[weak self] data, response, error in
-                
-                if let error = error {
-                //MARK: 에러 처리
-                }
-                
-                guard let response = response as? HTTPURLResponse,
-                      (200...299).contains(response.statusCode)
-                else{
-                    //MARK: 에러 처리
-                    return
-                }
-                
-                if let data = data,
-                   let MovieDetail = try? JSONDecoder().decode(MovieDetail.self, from: data) {
-                    
+        getDetailMovie()
+        titleLabel.backgroundColor = UIColor.clear
+        titleLabel.textColor = UIColor(named: "LabelTextColor")
+        stackView.backgroundColor = UIColor.clear
+        scrollView.backgroundColor = UIColor.clear
+        descriptionLabel.backgroundColor = UIColor.clear
+        descriptionLabel.textColor = UIColor(named: "LabelTextColor")
+        ratingLabel.backgroundColor = UIColor.clear
+        ratingLabel.textColor = UIColor(named: "LabelTextColor")
+        releasedateLabel.backgroundColor = UIColor.clear
+        releasedateLabel.textColor = UIColor(named: "LabelTextColor")
+        reservationButton.backgroundColor = UIColor(named: "customPrimaryColor")
+        
+        reservationButton.layer.cornerRadius = 10
+        reservationButton.backgroundColor = UIColor.blue
+        reservationButton.setTitleColor(UIColor.white, for: .normal)
+        
+    }
+    
+    func getDetailMovie() {
+        MovieListApiManager.shared.getMovieDetail(id: 693134) { result in
+            switch result {
+            case .success(let movies):
+                print("MovieDetail: \(movies)")
+                self.movieDetail = movies
+                if let movieDetail = self.movieDetail {
+                    print("영화 제목입니다!!!!!!!!!!!!!!!! \(movieDetail.title)")
                     DispatchQueue.main.async {
-                        self?.titleLabel.text = MovieDetail.movieTitle
-                        self?.detailLabel.text = MovieDetail.movieDetail
-                        self?.ratedLabel.text = "\(MovieDetail.rating)"
-                        self?.releasedateLabel.text = "\(MovieDetail.releaseDate)"
-                    }
-                    
-                    let imageRequest = URLRequest(url: url)
-                    let imageDataTask = URLSession.shared.dataTask(with: imageRequest) { data, response, error in
-                        
-                        if let error = error {
-                        //MARK: 에러 처리
-                        }
-                        
-                        guard let response = response as? HTTPURLResponse,
-                              (200...299).contains(response.statusCode)
-                        else{
-                            //MARK: 에러 처리
-                            return
-                        }
-                        
-                        if let data = data{
-                            
-                            DispatchQueue.main.async{
-                                let image = UIImage(data: data)
-                                self?.detailimageView.image = image
-                            }
+                        self.titleLabel.text = movieDetail.title
+                        self.descriptionLabel.text = movieDetail.overview
+                        self.ratingLabel.text = String(format: "%.1f", movieDetail.voteAverage)
+                        self.releasedateLabel.text = movieDetail.releaseDate
+                        if let posterPath = movieDetail.posterPath {
+                            self.detailimageView.image = UIImage(named: posterPath)
                         }
                     }
-                    print(MovieDetail.movieTitle)
-                    print(MovieDetail.movieDetail)
-                    print(MovieDetail.movieImage)
-                    print(MovieDetail.rating)
-                    print(MovieDetail.releaseDate)
-                    
                 }
-                
+            case .failure(let error):
+                print(error)
             }
-                task.resume()
-                
-            }
-            
-            // Label 초기화
-            let descriptionLabel = UILabel()
-        descriptionLabel.text = "\("이거 바뀌나?")"
-
-
-
-
-
-
-
-        descriptionLabel.numberOfLines = 0 // 여러 줄로 텍스트를 표시할 수 있도록 설정
-        descriptionLabel.sizeToFit() // 라벨의 크기를 텍스트에 맞게 자동으로 조정
-            
-            
-//            // Label을 ScrollView에 추가합니다.
-//            scrollView.addSubview(detailLabel)
-//
-//            // ScrollView를 뷰에 추가합니다.
-//            view.addSubview(scrollView)
-//
-            
-            
+        }
+        
+        
+        // 컨텐츠의 크기 설정 (API에서 가져올 때 동적으로 설정)
+        scrollView.contentSize = CGSize(width: 393, height: 680)
+        
+        // 스크롤뷰의 스크롤 가능 여부 설정
+        scrollView.isScrollEnabled = true
+        
+        // 스크롤뷰의 delegate 설정
+        scrollView.delegate = self
+        
+        // API로부터 데이터를 받아와서 스크롤뷰에 추가하는 함수 호출
+        getDataAndUpdateScrollView()
+        
+        
+        // UIScrollViewDelegate 메서드 구현
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            // 스크롤이 발생할 때 호출되는 메서드
+            print("스크롤 발생")
+        }
+        
+        // API로부터 데이터를 받아와서 스크롤뷰에 추가하는 함수
+        func getDataAndUpdateScrollView() {
         }
     }
+}
+
+
 
 
 
